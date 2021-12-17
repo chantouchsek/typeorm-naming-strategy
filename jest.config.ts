14c919e4899b482b51dfff0e418ab97db1021e8d
Touch <chantouchsek.cs83@gmail.com>
2021-12-17 21:48:21 +0700

    fix: :beer: fixed eslint run

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,8 @@
-import type { InitialOptionsTsJest } from 'ts-jest/dist/types'
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
 
 const config: InitialOptionsTsJest = {
   roots: ['<rootDir>/tests'],
   preset: 'ts-jest',
   testEnvironment: 'node',
-}
-export default config
+};
+export default config;
